2e7ee4452bb3083043a8a16165d276054531b289
TanvirMain49 <tanvirmahin607@gmail.com>
2025-12-14 23:08:08 +0600

    feat: add morgan for HTTP request logging and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended:true, limit: "16kb" })); //encode the url like instead of %20 it will use +
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(morgan("tiny"));
 
 
 
